ec84f31e43bafec0a0f758c62edc593c15077fd4
nikhilbaloji <nikhilbaloji@gmail.com>
2025-12-02 11:51:51 +0530

    Create vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,22 @@
+def vehicle_info(vehicle_number, owner_name, vehicle_type, year_of_manufacture):
+    result = (
+        f"Vehicle Number: {vehicle_number}\n"
+        f"Owner Name: {owner_name}\n"
+        f"Vehicle Type: {vehicle_type}\n"
+        f"Year of Manufacture: {year_of_manufacture}"
+    )
+    return result
+
+
+def main():
+   
+    vehicle_number = "AB12CD3456"
+    owner_name = "Nikhil"
+    vehicle_type = "car"
+    year_of_manufacture = 2025
+
+    print(vehicle_info(vehicle_number, owner_name, vehicle_type, year_of_manufacture))
+
+
+if _name_ == "_main_":
+    main()
